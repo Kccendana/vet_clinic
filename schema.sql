1684b71e26778eec7d61d00b33d3391b55abd757
kcccendana <kc_telyn@yahoo.com>
2023-09-20 23:17:10 +0900

    create species and owners table

--- schema.sql
+++ schema.sql
@@ -10,3 +10,24 @@ CREATE TABLE animals (
 );
 
 ALTER TABLE animals ADD species varchar(255);
+
+CREATE TABLE owners (
+    id serial PRIMARY KEY,
+    full_name varchar(255),
+    age INTEGER
+);
+
+CREATE TABLE species (
+    id serial PRIMARY KEY,
+    name varchar(255)
+);
+
+ALTER TABLE animals
+    DROP species,
+    ADD COLUMN species_id INTEGER,
+    ADD COLUMN owner_id INTEGER;
+
+ALTER TABLE animals
+    ADD CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCES species(id),
+    ADD CONSTRAINT fk_owner FOREIGN KEY (owner_id) REFERENCES owners(id);
+    
